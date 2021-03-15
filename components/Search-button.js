edc1a09058d4c9fc258a1622f0ea94360f020f62
Kat Maskell <kat.maskell@clearscore.com>
2021-03-15 08:56:52 +0000

    feat(searchButton): add search & submit component logic

--- components/Search-button.js
+++ components/Search-button.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
-const SearchButton = () => {
+const SearchButton = (props)  => {
+    // using state to save search value input
+    const [searchInput, setSearchInput] = useState("");
+
+    // takes input, saves to state
+    const handleSearchInputChanges = (event) => {
+        setSearchInputValue(event.target.value);
+    }
+
+    // resets input field to empty string
+    const resetSearchInputField = () => {
+        setSearchInput("");
+    }
+    // gets searchValue from props, preventDefault prevents a browser refresh
+    const callSearchFunction = (event) => {
+        event.preventDefault();
+        props.search(searchInput);
+        resetSearchInputField();
+    }
+
 
    return (
      <form>
-       <input placeholder="Search a film..."/>
+       <input
+       value={"avengers"}
+       onChange={handleSearchInputChanges}
+       type="text"
+       placeholder="Search a film..."/>
+       <input onCLick={callSearchFunction} type="submit" value="FIND MY MOVIE!" />
      </form>
    )
 }
