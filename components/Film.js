f7070ffb95283cee6cb954590f7da46d24d9352a
Kat Maskell <kat.maskell@clearscore.com>
2021-03-17 19:09:13 +0000

    feat(filmcard): add new film component

---
+++ components/Film.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from '../styles/Film.module.css';
+import Image from 'next/image';
+
+const DEFAULT_PLACEHOLDER_IMAGE = `/thumbnailPlaceholder.jpeg`;
+
+const Film = ({ film }) => {
+    const thumbnail =
+    film.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : film.Poster;
+    return (
+        <div className={styles.filmCard}>
+            <img 
+            className={styles.thumbnail}
+            width="50"
+            alt={`(${film.Title} thumbnail)`}
+            src={thumbnail}
+            />
+            <p className={styles.title}>{film.Title}</p>
+        </div>
+    );
+};
+
+export default Film;
